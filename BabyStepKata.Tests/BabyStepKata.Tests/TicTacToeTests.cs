43e513fb69771102629a64e5dd9178024704f1f7
mathieu-decroocq-econocom <mathieu.decroocq@econocom.com>
2018-10-15 13:24:16 +0200

    update game test

--- BabyStepKata.Tests/BabyStepKata.Tests/TicTacToeTests.cs
+++ BabyStepKata.Tests/BabyStepKata.Tests/TicTacToeTests.cs
@@ -29,14 +29,37 @@ namespace TicTacToeKata.Tests
             // ..0
             // 00.
             // XXX
+            Game game = new Game();
+            game.Play("0", 1, 1);
+            game.Play("X", 1, 2);
+            game.Play("0", 2, 0);
+            game.Play("X", 2, 2);
+            game.Play("0", 0, 1);
+            game.Play("X", 0, 2);
+            
+
+
+            // P1 Wins!
+            Assert.True(game.GetWinner() == "X");
         }
 
         [Fact]
-        public void Game_P1Wins_AllMarkersColumn()
+        public void Game_P2Wins_AllMarkersColumn()
         {
-            // 0..
-            // XXX
-            // .0.
+            // 0XX
+            // 00.
+            // 0.X
+            Game game = new Game();
+            game.Play("X", 1, 0);
+            game.Play("0", 0, 0);
+            game.Play("X", 2, 0);
+            game.Play("0", 0, 1);
+            game.Play("X", 2, 2);
+            game.Play("0", 0, 2);
+            
+
+            // P2 Wins!
+            Assert.True(game.GetWinner() == "Y");
         }
 
         [Fact]
