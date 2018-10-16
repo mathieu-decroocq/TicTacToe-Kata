ce313c12d39d90289ac4f3ad4cda9bd1f0d716bb
mathieu-decroocq-econocom <mathieu.decroocq@econocom.com>
2018-10-16 14:08:56 +0200

    get winner ok if all marker in row

--- BabyStepKata.Tests/BabyStepKata.Tests/TicTacToeTests.cs
+++ BabyStepKata.Tests/BabyStepKata.Tests/TicTacToeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using TicTacToeKata.Lib;
 
@@ -6,60 +7,68 @@ namespace TicTacToeKata.Tests
 {
     public class TicTacToeTests
     {
-        [Fact]
-        public void Game_P1Wins_AllMarkersDiagonale()
+        public class GameTests
         {
-            // X..
-            // 0X0
-            // ..X
-            Game game = new Game();
-            game.Play("X", 0, 0);
-            game.Play("0", 0, 1);
-            game.Play("X", 1, 1);
-            game.Play("0", 2, 1);
-            game.Play("X", 2, 2);
 
-            // P1 Wins!
-            Assert.True(game.GetWinner() == "X");
-        }
+            [Fact]
+            public void P1Wins_AllMarkersLine()
+            {
+                // ..0
+                // 00.
+                // XXX
+               // var board = new[,] { { "", "", "0" }, { "0", "0", "" }, { "X", "X", "X" } };
+                var board = new List<string[]>
+                {
+                    new [] {"", "", "0"},
+                    new [] {"0", "0", ""},
+                    new [] {"X", "X", "X"}
+                };
+                Game game = new Game(board);
 
-        [Fact]
-        public void Game_P1Wins_AllMarkersLine()
-        {
-            // ..0
-            // 00.
-            // XXX
-            Game game = new Game();
-            game.Play("0", 1, 1);
-            game.Play("X", 1, 2);
-            game.Play("0", 2, 0);
-            game.Play("X", 2, 2);
-            game.Play("0", 0, 1);
-            game.Play("X", 0, 2);
-            
+                // P1 Wins!
+                Assert.True(game.GetWinner() == "X");
+            }
 
+            [Fact]
+            public void P1Wins_AllMarkersDiagonale()
+            {
+                // X..
+                // 0X0
+                // ..X
+                // var board = new [,] { { "X", "", "" }, { "0", "X", "0" }, { "", "", "X" } };
+                var board = new List<string[]>
+                {
+                    new [] {"X", "", "" },
+                    new [] {"0", "X", "0"},
+                    new [] {"", "", "X"}
+                };
 
-            // P1 Wins!
-            Assert.True(game.GetWinner() == "X");
-        }
+                Game game = new Game(board);
+  
+                // P1 Wins!
+                Assert.True(game.GetWinner() == "X");
+            }
 
-        [Fact]
-        public void Game_P2Wins_AllMarkersColumn()
-        {
-            // 0XX
-            // 00.
-            // 0.X
-            Game game = new Game();
-            game.Play("X", 1, 0);
-            game.Play("0", 0, 0);
-            game.Play("X", 2, 0);
-            game.Play("0", 0, 1);
-            game.Play("X", 2, 2);
-            game.Play("0", 0, 2);
-            
+            [Fact]
+            public void P2Wins_AllMarkersColumn()
+            {
+                // 0XX
+                // 00.
+                // 0.X
+                //  var board = new[,] { { "0", "X", "X" }, { "0", "0", "" }, { "0", "", "X" } };
+                var board = new List<string[]>
+                {
+                    new [] {"0", "X", "X"},
+                    new [] {"0", "0", ""},
+                    new [] {"0", "", "X"}
+                };
+                Game game = new Game(board);
 
-            // P2 Wins!
-            Assert.True(game.GetWinner() == "Y");
+                // P2 Wins!
+                Assert.True(game.GetWinner() == "0");
+            }
+
+    
         }
 
         [Fact]
@@ -67,7 +76,7 @@ namespace TicTacToeKata.Tests
         {
             Game game = new Game();
             game.Play("X", 0, 0);
-            
+
             Assert.Throws<InvalidOperationException>(() => game.Play("X", 1, 0));
         }
 
--- src/TicTacToeKata.Lib/Game.cs
+++ src/TicTacToeKata.Lib/Game.cs
@@ -1,14 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TicTacToeKata.Lib
 {
     public class Game
     {
         private string previousMarker;
-        private string[,] board = new string[3,3];
+      //  private string[,] board = new string[3,3];
+        private List<string[]> board = new List<string[]>(3);
+
 
         public Game()
         {
+        }
+
+        public Game(List<string[]> board)
+        {
+            this.board= board;
         }
 
         public void Play(string marker, int x, int y)
@@ -18,18 +27,24 @@ namespace TicTacToeKata.Lib
                 throw new InvalidOperationException("Same_Player_Play_Twice");
             }
 
-            if (board[x, y] != null)
+            //if (board[x, y] != null)
+            //{
+            //    throw new InvalidOperationException("Marker_Already_Placed");
+            //}
+
+            if (board[x][y] != null)
             {
                 throw new InvalidOperationException("Marker_Already_Placed");
             }
 
             previousMarker = marker;
-            board[x, y] = marker;
+            //board[x, y] = marker;
+            board[x][y] = marker;
         }
 
         public string GetWinner()
         {
-            return "X";
+            return board.First(b => b.All(m => m == "X") || b.All(m => m == "0"))[0];
         }
     }
 }
